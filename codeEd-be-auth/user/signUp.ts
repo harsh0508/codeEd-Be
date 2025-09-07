44715922633c16fcfea308dc062cf3dbba5224fb
Harsh0508 <Harsh.jvm@gmail.com>
2025-09-07 16:10:27 +1000

    auth setup

--- codeEd-be-auth/user/signUp.ts
+++ codeEd-be-auth/user/signUp.ts
@@ -1,10 +1,18 @@
 import  { type Request, type Response } from 'express';
+import * as jwt from 'jsonwebtoken';
 
 
 export const userController ={
 
 
     signUp : (req:Request,res:Response)=>{
+        const userName = req.body.userName
+        const passWord = req.body.passWord
+
+        const JwtKey = process.env.JWT_SECRET_KEY
+        if(JwtKey){
+            jwt.sign(userName , JwtKey)
+        }
         res.send("this is for signup new")
     }
 }
